536132091cd257420e9633e3a64371b10afdf4fb
Lucy Guzmán <analucygdl@gmail.com>
2021-08-18 17:04:19 -0500

    Solución de error de validación

--- prueba.js
+++ prueba.js
@@ -1,1 +1,14 @@
 console.log('Hello');
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.on('ready', readyDiscord);
+
+function readyDiscord(){
+    console.log('Aloh');
+}
+
+client.login('ODc3Mjc0MTA2MjU1NTExNjEz.YRwPLQ.qAB1GHZIdNNfqKUgB17HvKILyLE');
